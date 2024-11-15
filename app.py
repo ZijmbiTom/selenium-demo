1628a2d74c7ad70b07e71275973ce2bf39efe68b
Tom Greeven <tom.greeven@zijm-bi.nl>
2024-11-15 12:33:00 +0100

    paar aanpassingen

--- app.py
+++ app.py
@@ -17,8 +17,12 @@ def get_images_from_url(url):
         # Selenium WebDriver configureren met headless Chrome-opties
         options = webdriver.ChromeOptions()
         #options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--window-size=1920,1200')
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--remote-debugging-port=9222")  # Voegt een debugging-poort toe
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
         # Laad de pagina en haal de HTML op
