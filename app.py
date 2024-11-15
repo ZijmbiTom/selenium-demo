0ac3acdb2167a0a37b53a5671a091e7b1fde3a17
Tom Greeven <tom.greeven@zijm-bi.nl>
2024-11-15 12:29:06 +0100

    paar aanpassingen

--- app.py
+++ app.py
@@ -15,11 +15,10 @@ def get_images_from_url(url):
     driver = None
     try:
         # Selenium WebDriver configureren met headless Chrome-opties
-        options = Options()
-        #options.add_argument('--headless')
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
         options.add_argument('--disable-gpu')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--window-size=1920,1200')
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
         # Laad de pagina en haal de HTML op
